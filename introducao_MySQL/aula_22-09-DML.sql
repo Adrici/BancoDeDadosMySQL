2b64fd08c845a024d79361dc5fbc591a9a8a574f
cirellia5@gmail.com <cirellia5@gmail.com>
2020-09-22 11:26:44 -0300

    introducao_MySQL

--- introducao_MySQL/aula_22-09-DML.sql
+++ introducao_MySQL/aula_22-09-DML.sql
@@ -1,17 +1,14 @@
 create database db_deletar;
 use db_deletar;
-
 create table consumidores (
 	id int PRIMARY KEY AUTO_INCREMENT,
     nome varchar(30)
-
-
 );
 
 insert into consumidores (nome) values
-("Marcelo"),
 ("Julia");
 
 select* from consumidores; 
 delete from consumidores where id = 1;
 select * from consumidores; 
+
